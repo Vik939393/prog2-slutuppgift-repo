565c78d52b5270559bfd4812f0164847fe5dc064
Adrian Nötzel <adrian.noetzel@hotmail.com>
2026-05-08 16:20:57 +0200

    remove created

--- backend/src/main/java/se/su/inlupp/ListGraph.java
+++ backend/src/main/java/se/su/inlupp/ListGraph.java
@@ -1,35 +1,37 @@
 package se.su.inlupp;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import org.w3c.dom.Node;
+
+import java.util.*;
 
 public class ListGraph<T> implements Graph<T> {
+  private final Map<T, Set<Edge>> nodesWithEdges = new HashMap<>();
 
   @Override
   public void add(T node) {
-    throw new UnsupportedOperationException("Unimplemented method 'add'");
+    nodesWithEdges.putIfAbsent(node, new HashSet<Edge>());
   }
 
   @Override
   public void remove(T node) {
-    throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    if (nodesWithEdges.containsKey(node)) {
+      for (T n : nodesWithEdges.keySet()) {
+        nodesWithEdges.get(n).remove(node);
+      }
+      Set<Edge> viktor = nodesWithEdges.remove(node);
+    } else {
+      throw new NoSuchElementException();
+    }
   }
 
   @Override
   public boolean hasNode(T node) {
-    double random = Math.random();
-    if (random > 10) {
-        return false;
-    }
-    else {
-        throw new UnsupportedOperationException("Unimplemented method 'hasNode'");
-    }
-
+          return nodesWithEdges.containsKey(node);
   }
 
   @Override
   public void connect(T node1, T node2, String name, int weight) {
+
     throw new UnsupportedOperationException("Unimplemented method 'connect'");
   }
 
