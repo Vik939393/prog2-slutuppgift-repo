ea946f334bbee478bc36188dc094f97a5cdea34c
viktorspasov <v.spasov@yahoo.se>
2026-05-19 14:21:55 +0200

    påbörjad start()

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -4,27 +4,34 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Gui extends Application {
 
   public void start(Stage stage) {
-    Graph<String> graph = new ListGraph<String>();
-    String javaVersion = System.getProperty("java.version");
-    String javafxVersion = System.getProperty("javafx.version");
-    Label label =
-        new Label("Hello, ADRIAN NUTTSERVE " + javafxVersion + ", running on CHILLMODE " + javaVersion + ".");
+      stage.setTitle("BERRYS AND SHROOOOMS");
+      Graph<String> graph = new ListGraph<String>();
+      String javaVersion = System.getProperty("java.version");
+      String javafxVersion = System.getProperty("javafx.version");
+      Label label =
+              new Label("Hello, ADRIAN " + javafxVersion + ", running on CHILLMODE " + javaVersion + ".");
 
-    VBox root = new VBox(30, label);
-    root.setAlignment(Pos.CENTER);
-    Scene scene = new Scene(root, 640, 480);
-    stage.setScene(scene);
-    stage.show();
+      BorderPane root = new BorderPane();
+      root.setStyle("-fx-font-weight: bold");
+
+      Label top = new Label("BERRYS AND SHROOOOOMS");
+      //top.setAlignment(Pos.TOP_RIGHT);
+      top.setPrefWidth(200);
+      BorderPane.setAlignment(top, Pos.TOP_CENTER);
+      root.setTop(top);
+      Scene scene = new Scene(root, 640, 480);
+      stage.setScene(scene);
+      stage.show();
   }
 
   public static void main(String[] args) {
-
     launch(args);
   }
 }
