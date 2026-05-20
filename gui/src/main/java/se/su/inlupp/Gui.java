296e9b1273dfb948ee1477bfc762603af9eaa6ef
Adrian Nötzel <adrian.noetzel@hotmail.com>
2026-05-20 11:49:42 +0200

    buttons created

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -1,10 +1,15 @@
 package se.su.inlupp;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,17 +20,53 @@ public class Gui extends Application {
       Graph<String> graph = new ListGraph<String>();
       String javaVersion = System.getProperty("java.version");
       String javafxVersion = System.getProperty("javafx.version");
-      Label label =
-              new Label("Hello, ADRIAN " + javafxVersion + ", running on CHILLMODE " + javaVersion + ".");
+
+
 
       BorderPane root = new BorderPane();
       root.setStyle("-fx-font-weight: bold");
 
-      Label top = new Label("BERRYS AND SHROOOOOMS");
-      //top.setAlignment(Pos.TOP_RIGHT);
-      top.setPrefWidth(200);
-      BorderPane.setAlignment(top, Pos.TOP_CENTER);
-      root.setTop(top);
+      HBox topBar = new HBox(5);
+      HBox bottomBar = new HBox(5);
+
+      Button newButton = new Button("New Node");
+      Button openButton = new Button("Open");
+      Button saveButton = new Button("Save");
+      Button exitButton = new Button("Exit");
+      TextField textField = new TextField();
+      Button searchButton = new Button("Search");
+      Label resultLabel = new Label();
+      //resultLabel.prefWidth(100);
+      root.setBottom(resultLabel);
+
+
+      topBar.getChildren().addAll(
+              saveButton,
+              newButton,
+              openButton,
+              exitButton,
+              textField,
+              searchButton
+
+      );
+      bottomBar.getChildren().addAll(
+              resultLabel
+
+      );
+      /*searchButton.setOnAction(new EventHandler<ActionEvent>() {
+          @Override
+          public void handle(ActionEvent event) {
+            resultLabel.setText("Hej " + textField.getText());
+          }
+      });*/
+        searchButton.setOnAction(event -> {resultLabel.setText("Hej " + textField.getText());});
+
+
+
+      topBar.setAlignment(Pos.CENTER);
+      bottomBar.setAlignment(Pos.CENTER);
+      root.setTop(topBar);
+      root.setBottom(bottomBar);
       Scene scene = new Scene(root, 640, 480);
       stage.setScene(scene);
       stage.show();
